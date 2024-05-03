c5883b164f06e65fd4f44bf846b770729db6223c
muzhawir <muzhawir.amri@gmail.com>
2024-05-03 11:53:24 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Stringly
+
+Stringly is an Elixir package with a wide variety of string manipulation functions. Inspired by Laravel's Str helper, it enhances Elixir's built-in String module with additional practical tools.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `stringly` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:stringly, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at <https://hexdocs.pm/stringly>.
---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule Stringly.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :stringly,
+      version: "0.0.1",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Stringly",
+      source_url: "https://github.com/muzhawir/stringly"
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:styler, "~> 0.11", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "Elixir package with a wide variety of string manipulation functions. Inspired by Laravel's Str helper, it enhances Elixir's built-in String module with additional practical tools."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/muzhawir/bmkg_open_data"}
+    ]
+  end
+end
