48c0c4d6fc4b7bb42a1dba85901dae872aac6449
muzhawir <muzhawir.amri@gmail.com>
2024-05-07 20:14:49 +0800

    config: update package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stringly.MixProject do
   def project do
     [
       app: :stringly,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: description(),
