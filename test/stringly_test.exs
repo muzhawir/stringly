3822fd9ddbc22d660604a1dae2e51d0879476827
muzhawir <muzhawir.amri@gmail.com>
2024-05-11 15:22:53 +0800

    feat: add `Stringly.slug/5` test

--- test/stringly_test.exs
+++ test/stringly_test.exs
@@ -3,7 +3,7 @@ defmodule StringlyTest do
 
   doctest Stringly
 
-  describe "Stringly.limit/3" do
+  describe "Stringly.limit" do
     test "returns the original string when no limit is provided and string is shorter than default limit" do
       assert Stringly.limit("Hello world") === "Hello world"
     end
@@ -17,7 +17,7 @@ defmodule StringlyTest do
     end
   end
 
-  describe "Stringly.remainder_after/2" do
+  describe "Stringly.remainder_after" do
     test "return the remainder of a string after the first occurrence of a given value" do
       assert Stringly.remainder_after("Elixir is functional programming language", "is functional") ===
                "programming language"
@@ -29,7 +29,24 @@ defmodule StringlyTest do
     end
   end
 
-  describe "Stringly.words/3" do
+  describe "Stringly.slug" do
+    test "return url friendly slug from the given string" do
+      assert Stringly.slug("Post #1: Is Elixir A Functional Programming Language?") ===
+               "Post-1-Is-Elixir-A-Functional-Programming-Language"
+    end
+
+    test "return downcase url friendly slug from the given string" do
+      assert Stringly.slug("Post #1: Is Elixir A Functional Programming Language?", :downcase) ===
+               "post-1-is-elixir-a-functional-programming-language"
+    end
+
+    test "return upcase url friendly slug from the given string" do
+      assert Stringly.slug("Post #1: Is Elixir A Functional Programming Language?", :upcase) ===
+               "POST-1-IS-ELIXIR-A-FUNCTIONAL-PROGRAMMING-LANGUAGE"
+    end
+  end
+
+  describe "Stringly.words" do
     test "returns the original words when no limit no limit is provided and words is shorter than default limit" do
       assert Stringly.words("Elixir is a functional programming language") ===
                "Elixir is a functional programming language"
